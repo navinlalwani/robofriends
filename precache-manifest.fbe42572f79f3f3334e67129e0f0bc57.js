e9e01a6d0b50e1481a27127ab9f846490f27a68a
Navin Lalwani <navin@MacBook-Air-4.local>
2020-04-04 02:00:36 +0530

    Updates

---
+++ precache-manifest.fbe42572f79f3f3334e67129e0f0bc57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607c93755b1a61ff0c866179bf82654d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0217e0fbb92d7b0f747b",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "875fad52b7761beea8d8",
+    "url": "/robofriends/static/css/main.30407267.chunk.css"
+  },
+  {
+    "revision": "0217e0fbb92d7b0f747b",
+    "url": "/robofriends/static/js/2.f68723af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f68723af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "875fad52b7761beea8d8",
+    "url": "/robofriends/static/js/main.d190ca17.chunk.js"
+  },
+  {
+    "revision": "dab07940c1980e5aab1e",
+    "url": "/robofriends/static/js/runtime-main.a4afcfec.js"
+  }
+]);
